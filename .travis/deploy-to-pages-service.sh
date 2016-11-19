12d83ae2be621b422f9862612e009a01d4fcff3e
Xiao Hanyu <xiaohanyu1988@gmail.com>
2016-11-19 18:51:05 +0800

    Revert "Stick to pandoc 1.17.2, the code doesn't work for pandoc 1.18 now"
    This reverts commit 01cacfb556bcf4c2f05ee7108a748fc2814f3b82.

--- .travis/deploy-to-pages-service.sh
+++ .travis/deploy-to-pages-service.sh
@@ -6,7 +6,7 @@ rm -rf output
 # install the latest pandoc
 sudo apt-get update && sudo apt-get -y install jq
 # Get the latest .deb released.
-wget `curl https://api.github.com/repos/jgm/pandoc/releases/1.17.2 | jq -r '.assets[] | .browser_download_url | select(endswith("deb"))'` -O pandoc.deb
+wget `curl https://api.github.com/repos/jgm/pandoc/releases/latest | jq -r '.assets[] | .browser_download_url | select(endswith("deb"))'` -O pandoc.deb
 sudo dpkg -i pandoc.deb
 
 # build
